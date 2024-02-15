bbe06e5203e53ca84c062f120192cb91b53abfa6
Raiel Landre Godinho <raiellgod@gmail.com>
2024-02-15 16:04:48 -0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
-function novoEvento(){
+const calendar = document.querySelector(".calendar");
+const date = document.querySelector(".date");
+const daysContainer = document.querySelector(".days");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-    
-}
+
+let today = new Date();
+let activeDay;
+let month = today.getFullMonth();
+let yer = today.getFullYear();
+
+const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+];
+
